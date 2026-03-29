ebe9b9b89295fdbfbd7f1e4e6bf39c39b65910d9
Anatole-cyber-developper <anatoletrouche@icloud.com>
2026-03-29 23:11:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ function scanNetwork() {
   const loader = document.getElementById("loader");
   const result = document.getElementById("result");
 
-  // reset
   result.innerHTML = "";
   loader.classList.remove("hidden");
 
-  // simulation scan (2 secondes)
   setTimeout(() => {
 
     const networks = [
@@ -15,27 +13,30 @@ function scanNetwork() {
       { operator: "SFR", type: "5G", signal: -72 },
       { operator: "Bouygues", type: "4G", signal: -95 }
     ];
-    let bars = "";
 
-let level = 1;
-if (net.signal > -80) level = 4;
-else if (net.signal > -90) level = 3;
-else if (net.signal > -100) level = 2;
+    let output = "";
 
-for (let i = 1; i <= 4; i++) {
-  bars += `<div class="bar ${i <= level ? "active" : ""}"></div>`;
-}
+    networks.forEach(net => {
 
-output += `
-  <div class="card">
-    <h3>${net.operator}</h3>
-    <p>📡 ${net.type}</p>
-    <p>${net.signal} dBm</p>
-    <div class="signal">${bars}</div>
-  </div>
-`;
+      let bars = "";
+      let level = 1;
 
+      if (net.signal > -80) level = 4;
+      else if (net.signal > -90) level = 3;
+      else if (net.signal > -100) level = 2;
 
+      for (let i = 1; i <= 4; i++) {
+        bars += `<div class="bar ${i <= level ? "active" : ""}"></div>`;
+      }
+
+      output += `
+        <div class="card">
+          <h3>${net.operator}</h3>
+          <p>${net.type}</p>
+          <p>${net.signal} dBm</p>
+          <div class="signal">${bars}</div>
+        </div>
+      `;
     });
 
     loader.classList.add("hidden");
@@ -43,6 +44,8 @@ output += `
 
   }, 2000);
 }
+
+
 function toggleHacker() {
   document.body.classList.toggle("hacker");
 }
