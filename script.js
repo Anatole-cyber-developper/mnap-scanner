2c5ff0ba5e563ab4627c92a09b08580cdb59b842
Anatole-cyber-developper <anatoletrouche@icloud.com>
2026-03-29 22:41:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 function scanNetwork() {
-  console.log("Scan déclenché");
 
-  const result = document.getElementById("result");
+  const networks = [
+    { operator: "Orange", type: "4G", signal: -85 },
+    { operator: "SFR", type: "5G", signal: -72 },
+    { operator: "Bouygues", type: "4G", signal: -95 }
+  ];
 
-  if (!result) {
-    console.log("DIV result introuvable !");
-    return;
-  }
+  let output = "";
 
-  result.innerHTML = "<p>Test OK</p>";
+  networks.forEach(net => {
+
+    let quality = "📶 Mauvais";
+
+    if (net.signal > -80) quality = "🔥 Excellent";
+    else if (net.signal > -90) quality = "👍 Bon";
+
+    output += `
+      <div class="card">
+        <h3>${net.operator}</h3>
+        <p>📡 Type: ${net.type}</p>
+        <p>📶 Signal: ${net.signal} dBm</p>
+        <p>${quality}</p>
+      </div>
+    `;
+  });
+
+  document.getElementById("result").innerHTML = output;
 }
